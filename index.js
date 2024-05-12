2eb4bc9222df18f30503a4e6849e53dd580cf155
Ukulyelye <152890266+Ukulyelye@users.noreply.github.com>
2024-05-12 21:49:59 +0800

    register form redirect error fix

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 document.querySelectorAll('.link-option').forEach((e)=>{
     e.addEventListener('click', ()=>{
-        location.href = 'https://ukulyelye.github.io/FRP-bypass/Samsung/Register/signup-form-06/';
+        location.href = 'https://ukulyelye.github.io/FRP-bypass/account/Register/signup-form/';
     });
 })
 
